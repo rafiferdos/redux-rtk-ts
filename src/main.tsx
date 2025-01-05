d0a71445e7f682e2135af1ed3357e8d84a6b0548
Rafi Ferdos <rafiferdos@gmail.com>
2025-01-05 20:57:19 +0600

    wrap application in Redux Provider for state management

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { Provider } from 'react-redux'
 import { RouterProvider } from 'react-router-dom'
+import './index.css'
 import { ThemeProvider } from './providers/theme-provider.tsx'
+import { store } from './redux/store.ts'
 import routes from './routes/route.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      {/* <Provider store={''}> */}
-      <RouterProvider router={routes} />
-      {/* </Provider> */}
+      <Provider store={store}>
+        <RouterProvider router={routes} />
+      </Provider>
     </ThemeProvider>
   </StrictMode>
 )
